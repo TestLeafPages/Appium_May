efb26ac5984ecb5513b020194bcfd2a2e264d7ac
Gopinath Jayakumar <gopinath.jayakumar@testleaf.com>
2020-06-01 12:16:28 +0530

    week1.day2

--- src/main/java/week1/day2/TC003_Install_App.java
+++ src/main/java/week1/day2/TC003_Install_App.java
@@ -1,38 +1,40 @@
-package week1.day1;
+package week1.day2;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import io.appium.java_client.android.AndroidDriver;
 
-public class TC001_LaunchApp {
+public class TC003_Install_App {
 	
-	// if the app is already installed in mobile -> 
 	
-    //3 capabilities are mandatory
-				// appPackage
-				// appActivity
+// if the app is not install in mobile -> 
+	
+    //2 capabilities are mandatory
 				// deviceName
+				// app
     // Optional
 				// platform
+
 	
-	public static void main(String[] args) throws MalformedURLException {
+	public static void main(String[] args) throws MalformedURLException, InterruptedException {
 		
 		// Step01:- Set Desired Capabilities
 		DesiredCapabilities dc = new DesiredCapabilities();
-		dc.setCapability("appPackage", "com.android.dialer");
-		dc.setCapability("appActivity", "com.oneplus.contacts.activities.OPDialtactsActivity");
 		dc.setCapability("deviceName", "OnePlus 7T Pro");
 		dc.setCapability("platformName", "Android");
-		dc.setCapability("automationName", "UiAutomator2");
-		dc.setCapability("noReset", true);
+		dc.setCapability("app", "C:\\Users\\dell\\Desktop\\appium\\apks\\leaforg.apk");
 		
 		// Step02:- Create the driver and hit the appium server
 		AndroidDriver<WebElement> driver = new AndroidDriver<WebElement>(new URL("http://0.0.0.0:4723/wd/hub"), dc);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		
+		driver.findElementById("").sendKeys("rajkumar@testleaf.com");
+		driver.findElementById("").sendKeys("Leaf@123");
 		
 		
 		
--- src/main/java/week1/day2/TC003_Launch_Emulator.java
+++ src/main/java/week1/day2/TC003_Launch_Emulator.java
@@ -1,37 +1,42 @@
-package week1.day1;
+package week1.day2;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import io.appium.java_client.android.AndroidDriver;
 
-public class TC001_LaunchApp {
+public class TC003_Launch_Emulator {
 	
-	// if the app is already installed in mobile -> 
 	
-    //3 capabilities are mandatory
-				// appPackage
-				// appActivity
+// if the app is not install in mobile -> 
+	
+    //2 capabilities are mandatory
 				// deviceName
+				// app
     // Optional
 				// platform
+
 	
-	public static void main(String[] args) throws MalformedURLException {
+	public static void main(String[] args) throws MalformedURLException, InterruptedException {
 		
 		// Step01:- Set Desired Capabilities
 		DesiredCapabilities dc = new DesiredCapabilities();
-		dc.setCapability("appPackage", "com.android.dialer");
-		dc.setCapability("appActivity", "com.oneplus.contacts.activities.OPDialtactsActivity");
-		dc.setCapability("deviceName", "OnePlus 7T Pro");
+		dc.setCapability("deviceName", "Android Emulator");
+		dc.setCapability("platformVersion", "9.0");
 		dc.setCapability("platformName", "Android");
 		dc.setCapability("automationName", "UiAutomator2");
-		dc.setCapability("noReset", true);
+			
+		dc.setCapability("appPackage", "com.google.android.apps.messaging");
+		dc.setCapability("appActivity", "com.google.android.apps.messaging.ui.ConversationListActivity");
+		
 		
 		// Step02:- Create the driver and hit the appium server
 		AndroidDriver<WebElement> driver = new AndroidDriver<WebElement>(new URL("http://0.0.0.0:4723/wd/hub"), dc);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		
 		
 		
